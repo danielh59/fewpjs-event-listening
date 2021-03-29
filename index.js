ef43637996a97d9d55c2157e97c9b131a3fa4ac7
tak59 <tak59@live.com>
2021-03-28 22:18:33 -0400

    commit tree 4552695681a6f1550171a06ac579a9f4956704f9

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const input = document.getElementById('input');
 input.addEventListener('click', function(event) {
   alert('I was clicked!');
 }); }
+function greet(timeStr){
+  let nowTime = timeStr.split(':');
+  let nowHour = parseInt(nowTime[0]);
+  if (nowHour<12){
+    return "Good Morning";
+  } else if (nowHour>17){
+    return "Good Evening";
+  }
+  return "Good Afternoon";
+}
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
+}
